ce4587a878f778f8ac1a6e6956343b49b30b39c2
breadpakoda <adityasing272@gmail.com>
2026-02-11 12:09:48 +0530

    added the backend is running part for the process monitoring

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,9 @@ require("dotenv").config()
 const app = express()
 app.use(express.json())
 app.use(cors())
+app.get("/", (req, res) => {
+  res.status(200).send("Backend is running 🚀");
+});
 
 let db;
 
